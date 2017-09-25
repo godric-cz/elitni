b897dc74f96d15d3e8d8084fa2c8577f3154d553
Godric <godric@korh.cz>
2017-09-25 21:08:24 +0200

    kontroly při přihlašování

--- src/Aktivita.php
+++ src/Aktivita.php
@@ -2,8 +2,20 @@
 
 class Aktivita {
 
+    function kryjeSe(Aktivita $a) {
+
+    }
+
     function prihlas(Uzivatel $u) {
+        if(!$this->volnoPro($u))    throw new Plno;
+        if(!$u->maVolnoNa($this))   throw new PrekrytiAktivit;
+    }
+
+    private function volnoPro(Uzivatel $u) {
 
     }
 
 }
+
+class PrekrytiAktivit extends Exception {}
+class Plno extends Exception {}
--- src/Uzivatel.php
+++ src/Uzivatel.php
@@ -2,6 +2,17 @@
 
 class Uzivatel {
 
+    private function aktivity() {
+
+    }
+
+    function maVolnoNa(Aktivita $a) {
+        foreach($this->aktivity() as $stavajiciAktivita) {
+            if($stavajiciAktivita->kryjeSe($a)) return false;
+        }
+        return true;
+    }
+
     static function zMailu(string $mail) {
 
     }
