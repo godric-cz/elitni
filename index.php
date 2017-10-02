2261ffc692b1e98fa016cb0aeec4f9f4513200cd
Godric <godric@korh.cz>
2017-10-02 16:00:41 +0200

    hlavní stránka základ

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="soubory/styl.css">
+    <title></title>
+</head>
+<body>
+
+    <?php include('casti/hlavicka.html'); ?>
+    <?php include('stranky/hlavni.html'); ?>
+    <?php include('casti/paticka.html'); ?>
+
+</body>
+</html>
