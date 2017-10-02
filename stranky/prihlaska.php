bab721bf2b87596f0e39ea06e83abfc314f193a5
Godric <godric@korh.cz>
2017-10-02 17:06:51 +0200

    záhlaví přihlašování

--- stranky/prihlaska.php
+++ stranky/prihlaska.php
@@ -3,6 +3,7 @@
 
 
 ?>
+<div class="pruh" style="background-image: url(soubory/formular_uvodka.jpg); height: 300px; background-size: auto 400%; background-position: 50% 37%"></div>
 <div class="pruh">
     <div class="obsah">
         <?php include 'casti/prihlaska-text.html' ?>
