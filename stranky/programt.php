1b69666c26a976d43ee8b1794798fb257be983bd
Godric <godric@korh.cz>
2017-10-16 13:15:03 +0200

    reálný program z databáze do programt

--- stranky/programt.php
+++ stranky/programt.php
@@ -1,6 +1,17 @@
-<div class="pruh">
-  <div class="obsah">
-      <h2>Program</h2>
-          <img src="soubory/program.png" class="programt">
-  </div>
+<div class="pruh" style="padding-bottom: 200px">
+    <div class="obsah">
+        <h2 style="margin-bottom: 50px">Program</h2>
+
+        <div class="box2">
+            <div>
+                <div class="program">
+                    <?php
+                        $uzivatel = null;
+                        include 'casti/program-tabulka.php';
+                    ?>
+                </div>
+            </div>
+        </div>
+
+    </div>
 </div>
