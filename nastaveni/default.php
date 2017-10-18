f7589fa8074b8125e691cff0968e9df1905dc762
Godric <godric@korh.cz>
2017-10-18 20:18:05 +0200

    posun registrace pro 2. vlnu

--- nastaveni/default.php
+++ nastaveni/default.php
@@ -8,7 +8,7 @@ return [
         'database'  =>  'elitni',
     ],
     'errors'    =>  true,
-    'startRegu' =>  '2017-10-12 21:00',
+    'startRegu' =>  '2017-10-19 21:00',
     'hesloAdmin'=>  'tak určitě',
     'klicAdmin' =>  'tT6oGj9KcufqHYxj',
     'klicNasad' =>  'ffyIyYHlLZFVfO8f',
